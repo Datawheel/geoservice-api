3b5c3e8c30a91b753a37c20d94f4c17b98709c48
Jonathan Speiser <jspeis@gmail.com>
2017-05-05 09:02:10 -0400

    removes extra log statements

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
-import {version} from "../../package.json";
 import {Router} from "express";
+
+import {version} from "../../package.json";
 import levels from "../../db/levels.json";
 
 
-export default ({config, db}) => {
-  console.log(config, db);
+export default ({db}) => {
   const api = new Router();
 
   api.get("/", (req, httpResult) => {
@@ -27,14 +27,12 @@ export default ({config, db}) => {
 
     const targetTable = `${levels[level].schema}.${levels[level].table}`;
 
-    console.log(level, levels);
-
     const qry = `SELECT * from ${targetTable} s1,
               ${targetTable} s2
               WHERE ST_Touches(s1.geom, s2.geom)
               AND s1.geoid = $1
               LIMIT 10;`;
-              console.log(qry);
+
     db.query(qry, geoId)
     .then((results, error) => {
       httpResult.json({results, error});
