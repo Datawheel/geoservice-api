8e26229dd74110e1d71ea5f314b7eaef6793972c
Jonathan Speiser <jspeis@gmail.com>
2018-05-09 17:50:59 -0400

    updates for faf data

--- config/acs_levels.js
+++ config/acs_levels.js
@@ -1,29 +1,30 @@
 export const levels = {
   shapes: {
-    "state": {"schema": "shapes2017", "table": "states", "id": "geoid", "parent": "state", "columns": ["name", "geoid"], "srid": 4269},
-    "county": {"schema": "shapes2017", "table": "counties", "id": "geoid", "parent": "state", "columns": ["name", "geoid"], "nameColumn": "namelsad", "srid": 4269},
-    "place": {"schema": "shapes2017", "table": "places", "id": "geoid", "parent": "county", "columns": ["name", "geoid"], "srid": 4269},
-    "tract": {"schema": "shapes2017", "table": "tracts", "id": "geoid", "parent": "county", "columns": ["name", "geoid"], "nameColumn": "namelsad", "srid": 4269},
-    "msa": {"schema": "shapes2017", "table": "msas", "id": "geoid", "parent": "state", "columns": ["name", "geoid"], "srid": 4269},
-    "puma": {"schema": "shapes2017", "table": "pumas", "id": "geoid", "parent": "state", "columns": ["name", "geoid"], "srid": 4269}
+    state: {schema: "shapes2017", table: "states", id: "geoid", parent: "state", columns: ["name", "geoid"], srid: 4269},
+    county: {schema: "shapes2017", table: "counties", id: "geoid", parent: "state", columns: ["name", "geoid"], nameColumn: "namelsad", srid: 4269},
+    place: {schema: "shapes2017", table: "places", id: "geoid", parent: "county", columns: ["name", "geoid"], srid: 4269},
+    tract: {schema: "shapes2017", table: "tracts", id: "geoid", parent: "county", columns: ["name", "geoid"], nameColumn: "namelsad", srid: 4269, ignoreByDefault: true},
+    msa: {schema: "shapes2017", table: "msas", id: "geoid", parent: "state", columns: ["name", "geoid"], srid: 4269},
+    puma: {schema: "shapes2017", table: "pumas", id: "geoid", parent: "state", columns: ["name", "geoid"], srid: 4269, ignoreByDefault: true},
+    faf_region: {schema: "shapes2017", table: "faf_regions", id: "geoid", nameColumn: "f3_name", parent: "state", columns: ["f3_name", "geoid"], srid: 4269, ignoreByDefault: true}
   },
   points: {
-    "university": {
-      "schema": "ipeds_geo",
-      "table": "university_geom",
-      "id": "university",
-      "longitude": "lng",
-      "latitude": "lat",
-      "nameColumn": "university_name"}
+    university: {
+      schema: "ipeds_geo",
+      table: "university_geom",
+      id: "university",
+      longitude: "lng",
+      latitude: "lat",
+      nameColumn: "university_name"}
   },
   simpleRelations: {
-    "state": {
+    state: {
       lengthToRetain: 9,
-      "levels": ["county", "tract", "puma"]
+      levels: ["county", "tract", "puma"]
     },
-    "county": {
+    county: {
       lengthToRetain: 12,
-      "levels": ["tract"]
+      levels: ["tract"]
     }
   }
 };
