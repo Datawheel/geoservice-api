407d0feadef6e4c20ff1cc895d97db0c774b82ae
Jonathan Speiser <jspeis@gmail.com>
2018-08-02 10:05:59 -0400

    updates authority health configuration

--- config/ah_levels.js
+++ config/ah_levels.js
@@ -5,8 +5,8 @@ export default {
     place: {schema: "shapes2017", table: "places", id: "geoid", parent: "county", columns: ["name", "geoid"], srid: 4269},
     tract: {schema: "shapes2017", table: "tracts", id: "geoid", parent: "county", columns: ["name", "geoid"], nameColumn: "namelsad", srid: 4269, ignoreByDefault: true},
     msa: {schema: "shapes2017", table: "msas", id: "geoid", parent: "state", columns: ["name", "geoid"], srid: 4269},
-    puma: {schema: "shapes2017", table: "pumas", id: "geoid", parent: "state", columns: ["name", "geoid"], srid: 4269, ignoreByDefault: true},
-    zip: {schema: "shapes2017", table: "zips", id: "geoid", parent: "state", columns: ["zcta5ce10", "geoid"], srid: 4269, ignoreByDefault: true}
+    puma: {schema: "shapes2017", table: "pumas", id: "geoid", parent: "state", columns: ["name", "geoid"], srid: 4269, ignoreByDefault: true, nameColumn: "namelsad10"},
+    zip: {schema: "shapes2017", table: "zips", id: "geoid", parent: "state", columns: ["zcta5ce10", "geoid"], srid: 4269, ignoreByDefault: false}
   },
   points: {
   },
